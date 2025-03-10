ff0e7024b9edd8c62d65bf31dd76341ae530258c
SoldierB0y <jebatistav14@gmail.com>
2025-03-10 00:39:09 -0300

    my portfolio

---
+++ sources/js/index.js
@@ -0,0 +1,97 @@
+const card1=document.getElementById('card1');
+const card2=document.getElementById('card2');
+const list1= document.getElementById('list1');
+const list2=document.getElementById('list2');
+
+list1.style.opacity=0;
+list2.style.opacity=0;
+list1.style.transition='.3s ease-in-out'
+list2.style.transition='.3s ease-in-out'
+
+
+function hideText(list)
+{
+    
+    if(list=='list1')
+    {
+        list1.style.opacity=1;
+    }
+    else
+    {
+        list2.style.opacity=1;
+    }
+    
+}
+
+function showText(list)
+{
+    if(list=='list1')
+    {
+        list1.style.opacity=0;
+    }
+    else
+    {
+        list2.style.opacity=0;
+    }
+
+}
+
+card1.addEventListener('mouseenter',()=>{
+    hideText('list1');
+});
+
+card1.addEventListener('mouseleave',()=>{
+    showText('list1');
+});
+
+card2.addEventListener('mouseenter',()=>{
+    hideText('list2');
+});
+
+card2.addEventListener('mouseleave',()=>{
+    showText('list2');
+});
+
+
+//homeButton
+const homeButton= document.getElementById('homeButton');
+homeButton.style.opacity=0;
+var screenHeight= window.screen.height;
+
+var screenTop;
+
+
+document.addEventListener('wheel',()=>{
+    screenTop=$(document).scrollTop();
+    if(screenTop <= screenHeight)
+    {
+        homeButton.style.opacity=0;
+    }
+    else
+    {
+        homeButton.style.opacity=1;
+    }
+})
+
+
+//card
+
+const cards = document.querySelectorAll('.cardInfoImg');
+const cardText= document.querySelectorAll('.cardInfoSpanText')
+const canQuit= false;
+const startTimer=false;
+cards.forEach(card => {
+    card.addEventListener('mouseenter', () => {
+        card.classList.add('styleFlipLikeCard');
+        if(startTimer==false)
+        {
+            startTimer==true;
+            setTimeout(() => {
+                card.classList.remove('styleFlipLikeCard')
+            }, 5000);
+        }
+
+    });
+
+});
+
